19734320be5c10fd16b0be56ef6cd893518baec7
Miruna\MRN <vlaicu_miruna@yahoo.com>
2017-10-12 22:53:52 +0300

    a resemblance to brick breaker

--- ez-brick-breaker/src/brick-breaker/PublicFiles/Main.cpp
+++ ez-brick-breaker/src/brick-breaker/PublicFiles/Main.cpp
@@ -8,6 +8,8 @@
 #include <Rectangle.h>
 #include <Utils.h>
 
+#include <vector>
+
 using namespace SE;
 
 class Pad
@@ -18,7 +20,7 @@ public:
 		:renderer(renderer)
 		, velocityX(velocityX)
 		, velocityY(velocityY)
-		, pad(std::make_shared<SE::Rectangle>(vec3(5.0f, 10.0f, 0.0f), vec2(20, 5), vec3(132.0f, 100.0f, 255.0f), 0))
+		, pad(std::make_shared<SE::Rectangle>(vec3(5.0f, 190.0f, 0.0f), vec2(20, 5), vec3(132.0f, 100.0f, 255.0f), 0))
 	{
 		renderer->addToDrawCall(pad);
 	}
@@ -29,16 +31,29 @@ public:
 		return pad.get();
 	}
 
-	void move()
+	void moveRight()
 	{
 		auto origin = pad->getOrigin();
 		//std::cout << origin.x << std::endl;
-		if (!SE::Utils::inRange(origin.x, LEFT_MOVING_LIMIT, RIGHT_MOVING_LIMIT - pad->getWidth()))
+		/*if (!SE::Utils::inRange(origin.x, LEFT_MOVING_LIMIT, RIGHT_MOVING_LIMIT - pad->getWidth()))
 		{
 			velocityX = -velocityX;
-		}
+		}*/
 
-		//origin = origin.add(vec3(velocityX*0.1f, velocityY*0.0f, 0.0f));
+		origin = origin.add(vec3(velocityX*8.0f, velocityY*0.0f, 0.0f));
+		pad->translate(origin);
+	}
+
+	void moveLeft()
+	{
+		auto origin = pad->getOrigin();
+		//std::cout << origin.x << std::endl;
+		/*if (!SE::Utils::inRange(origin.x, LEFT_MOVING_LIMIT, RIGHT_MOVING_LIMIT - pad->getWidth()))
+		{
+		velocityX = -velocityX;
+		}*/
+
+		origin = origin.add(vec3(-velocityX*8.0f, velocityY*0.0f, 0.0f));
 		pad->translate(origin);
 	}
 
@@ -46,8 +61,6 @@ public:
 	{
 
 	}
-
-
 
 private:
 
@@ -57,9 +70,6 @@ private:
 	std::shared_ptr<SE::Rectangle> pad;
 	static const float LEFT_MOVING_LIMIT;
 	static const float RIGHT_MOVING_LIMIT;
-
-
-
 
 };
 const float Pad::LEFT_MOVING_LIMIT = 2.0f;
@@ -73,7 +83,7 @@ public:
 		: renderer(renderer)
 		, velocityX(velocityX)
 		, velocityY(velocityY)
-		, ball(std::make_shared<SE::Rectangle>(vec3(5.0f, 10.0f, 1.0f), vec2(5, 5), vec3(255.0f, 0.0f, 0.0f), 0))
+		, ball(std::make_shared<SE::Rectangle>(vec3(5.0f, 40.0f, 1.0f), vec2(5, 5), vec3(255.0f, 0.0f, 0.0f), 0))
 	{
 		renderer->addToDrawCall(ball);
 	}
@@ -91,15 +101,22 @@ public:
 		if (!SE::Utils::inRange(origin.x, LEFT_MOVING_LIMIT, RIGHT_MOVING_LIMIT - ball->getWidth()))
 		{
 			velocityX = -velocityX;
-			velocityY = -velocityY;
+			velocityY = velocityY;
 		}
 
 		origin = origin.add(vec3(velocityX*0.1f, velocityY* 0.1f, 0.0f));
 		ball->translate(origin);
 	}
-	void onCollision()
+	void onCollisionWithPad()
 	{
+		velocityY = -velocityY;
+		velocityX = velocityX;
+	}
 
+	void onCollisionWithBrick()
+	{
+		velocityY = -velocityY;
+		velocityX = velocityX;
 	}
 
 private:
@@ -115,6 +132,37 @@ private:
 
 const float Ball::LEFT_MOVING_LIMIT = 2.0f;
 const float Ball::RIGHT_MOVING_LIMIT = 200.0f;
+
+class Brick
+{
+
+public:
+	Brick(SimpleRenderer* renderer, vec3 position, vec2 size, vec3 color)
+		:renderer(renderer)
+		, brick(std::make_shared<SE::Rectangle>(position, size, color, 0))
+	{
+		renderer->addToDrawCall(brick);
+	}
+
+	~Brick() = default;
+
+	SE::Rectangle * getRectangle()
+	{
+		return brick.get();
+	}
+
+	void onCollisionWithBall()
+	{
+		brick->translate(vec3(1.0, 1.0, 0.0));
+	}
+
+private:
+
+	SimpleRenderer* renderer;
+	std::shared_ptr<SE::Rectangle> brick;
+};
+
+
 auto main() -> void
 {
 	auto window = std::make_unique<Window>("Test", 600, 800);
@@ -151,19 +199,53 @@ auto main() -> void
 	//auto y = glGetUniformLocation(shader.m_shaderID, "lpos");
 	//glUniform2f(y, 0.0f, 0.0f);
 	float inc = -0.1f;
+
+	int brickWidth = 15;
+	int spaceBetweenBricks = 5;
+	std::vector<Brick> bricks;
+	for (int i = 0; i < 10; i++)
+		bricks.push_back(Brick(&ren, vec3(i * brickWidth + i*spaceBetweenBricks, 20, 0), vec2(brickWidth, 4), vec3(0.0f, 1.0f, 0.0f)));
+
+	for (int i = 1; i < 9; i++)
+		bricks.push_back(Brick(&ren, vec3(i * brickWidth + i*spaceBetweenBricks, 30, 0), vec2(brickWidth, 4), vec3(1.0f, 0.0f, 1.0f)));
+
+
 	Pad pad(&ren);
+
+	SE::Rectangle*padEntity = pad.getRectangle();
+
+	InputManager::getInstance().init(window.get()->getWindow());
+	InputManager::getInstance().registerSpriteAction(std::bind(&Pad::moveRight, &pad), GLFW_KEY_D);
+	InputManager::getInstance().registerSpriteAction(std::bind(&Pad::moveLeft, &pad), GLFW_KEY_A);
+	/*InputManager::getInstance().registerSpriteAction(std::bind(&SE::Rectangle::moveRight, padEntity), GLFW_KEY_D);
+	InputManager::getInstance().registerSpriteAction(std::bind(&SE::Rectangle::moveUp, padEntity), GLFW_KEY_W);
+	InputManager::getInstance().registerSpriteAction(std::bind(&SE::Rectangle::moveDown, padEntity), GLFW_KEY_S);
+*/
+	/*auto funcPointer = static_cast<void(SE::Rectangle::*)(const vec3&)>(&SE::Rectangle::UpdateLocation);
+	InputManager::getInstance().registerSpriteAction(std::bind(funcPointer, s3.get(), s3->m_position.add(vec3(10, 10, 0.0f))), GLFW_KEY_S);
+*/
 	Ball ball(&ren);
+	SE::Rectangle* ballEntity = ball.getRectangle();
 	while (!window->closed())
 	{
 		window->clear();
-		pad.move();
+		//pad.move();
 		ball.move();
-		SE::Rectangle* a = ball.getRectangle();
-		SE::Rectangle*b = pad.getRectangle();
-		auto is = app.isCollided(a, b);
-		if (is)
+
+
+		if (app.isCollided(ballEntity, padEntity))
 		{
-			std::cout << 1 << std::endl;
+			ball.onCollisionWithPad();
+			std::cout << "COL";
+		}
+		for (auto& brick : bricks)
+		{
+			SE::Rectangle * brickEntity = brick.getRectangle();
+			if (app.isCollided(brickEntity, ballEntity))
+			{
+				ball.onCollisionWithBrick();
+				brick.onCollisionWithBall();
+			}
 		}
 		/*auto og = s1->getOrigin();
 		s1->translate(og.add(vec3(-inc*0.1f, -inc* 0.1f, 0.0f)));*/
