968494b9270a3e5923d4207e9a48e72e7404da33
Miruna\MRN <vlaicu_miruna@yahoo.com>
2017-10-18 20:03:04 +0300

    modified CollisionManager to take shared_ptr<Rectangle>

--- ez-brick-breaker/src/studious-engine/PublicFiles/CollisionManager.h
+++ ez-brick-breaker/src/studious-engine/PublicFiles/CollisionManager.h
@@ -13,14 +13,14 @@ namespace SE
 	public:
 		static auto getInstance()->CollisionManager&;
 		static auto addCollisionalEntities(std::shared_ptr<Rectangle> collidedObject,
-		                            std::shared_ptr<Rectangle> objectToColide,
-									std::function<void(Rectangle*, Rectangle*)>) -> void;
+			std::shared_ptr<Rectangle> objectToColide,
+			std::function<void(std::shared_ptr<Rectangle>, std::shared_ptr<Rectangle>)>) -> void;
 
 
 		static auto checkCollisions() -> void;
 
 	private:
-		static std::list<std::tuple<std::shared_ptr<Rectangle>, std::shared_ptr<Rectangle>, std::function<void(Rectangle*, Rectangle*)>>> m_collisionalEntities;
+		static std::list<std::tuple<std::shared_ptr<Rectangle>, std::shared_ptr<Rectangle>, std::function<void(std::shared_ptr<Rectangle>, std::shared_ptr<Rectangle>)>>> m_collisionalEntities;
 		static Application m_app;
 	};
 }
