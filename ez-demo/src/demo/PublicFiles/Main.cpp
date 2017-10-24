c50a4b9a13bbe05c9c74e8bd23521a4e5e5a1979
Miruna\MRN <vlaicu_miruna@yahoo.com>
2017-10-24 22:06:41 +0300

    ambitious remake demo

--- ez-demo/src/demo/PublicFiles/Main.cpp
+++ ez-demo/src/demo/PublicFiles/Main.cpp
@@ -7,101 +7,115 @@
 
 #include <memory>
 
-class MyGame : public SE::Game {
+using namespace SE;
+
+class MyDemoGame : public Game {
 
 public:
-	MyGame()
+	MyDemoGame() = default;
+	~MyDemoGame()
 	{
+		SoundManager::clean();
 	}
 
-	virtual auto update() -> void
-	{
-		SE::CollisionManager::checkCollisions();
-		SE::SoundManager::update();
-	}
 	virtual auto start() -> void
 	{
 		Game::init();
-		SE::InputManager::init(Game::m_window->getWindow());
-		SE::SoundManager::init();
-
-		texture1 = SE::Texture("..//..//src//demo//PublicFiles//Textures//texture1.png");
-		texture2 = SE::Texture("..//..//src//demo//PublicFiles//Textures//texture2.png");
-
-		rectangle1 = std::make_shared<SE::Rectangle>(SE::vec2(40, 40), SE::vec2(50, 50), texture1, 2);
-		rectangle2 = std::make_shared<SE::Rectangle>(SE::vec2(150, 40), SE::vec2(50, 50), texture2, 3);
-
-		sound1 = std::make_shared<SE::Sound>("sunet1", "..//..//src//demo//PublicFiles//Sounds//cat_growl.wav");
-		sound2 = std::make_shared<SE::Sound>("sunet2", "..//..//src//demo//PublicFiles//Sounds//dog_bark.wav");
-
-		Game::m_renderer->addRectangleToDrawCall(rectangle1);
-		Game::m_renderer->addRectangleToDrawCall(rectangle2);
-
-		SE::SoundManager::add(sound1);
-		SE::SoundManager::add(sound2);
+		loadTextures();
+		loadSounds();
+		initDrawableSprite();
 
 		glClearColor(1.0, 0.0, 0.0, 1.0);
 
-		bindKeyToRectangles();
-		bindRectangleToCollisionManager();
+		connectKeyPressedBehaviors();
+		connectOnCollisionBehaviors();
 		Game::run();
+	}
+
+
+private:
+	virtual auto update() -> void
+	{
+		SE::CollisionManager::checkCollisions();
+		SoundManager::update();
 	}
 
 	auto moveRectangle1()->void
 	{
-		rectangle1->translate(SE::vec3(rectangle1->getOrigin().x + 1, rectangle1->getOrigin().y, rectangle1->getPriority()));
+		auto newX = rectangle1->getOrigin().x + 1;
+		auto newY = rectangle1->getOrigin().y;
+		auto newPriority = rectangle1->getPriority();
+		rectangle1->translate(SE::vec3(newX, newY, newPriority));
 	}
 	auto moveRectangle2()->void
 	{
-		rectangle2->translate(SE::vec3(rectangle2->getOrigin().x - 1, rectangle2->getOrigin().y, rectangle2->getPriority()));
+		auto newX = rectangle2->getOrigin().x - 1;
+		auto newY = rectangle2->getOrigin().y;
+		auto newPriority = rectangle2->getPriority();
+		rectangle2->translate(SE::vec3(newX, newY, newPriority));
 	}
 
-	auto bindKeyToRectangles()->void
+	auto connectKeyPressedBehaviors()->void
 	{
-		SE::InputManager::registerSpriteAction(std::bind(&MyGame::moveRectangle1, this), GLFW_KEY_A);
-		SE::InputManager::registerSpriteAction(std::bind(&MyGame::moveRectangle2, this), GLFW_KEY_D);
+		SE::InputManager::init(Game::m_window->getWindow());
+		SE::InputManager::registerSpriteAction(std::bind(&MyDemoGame::moveRectangle1, this), GLFW_KEY_A);
+		SE::InputManager::registerSpriteAction(std::bind(&MyDemoGame::moveRectangle2, this), GLFW_KEY_D);
 	}
 
 	auto rectangleCollision(std::shared_ptr<SE::Rectangle> rectangle1, std::shared_ptr<SE::Rectangle> rectangle2)->void
 	{
-		static bool x = false;
-		if (x == false)
+		static bool isFirstCollision = true;
+		if (isFirstCollision)
 		{
-			SE::SoundManager::get("sunet1")->play();
-			SE::SoundManager::get("sunet2")->play();
-			x = true;
+			SoundManager::get("sunet1")->play();
+			SoundManager::get("sunet2")->play();
+			isFirstCollision = false;
 		}
 	}
 
-	auto bindRectangleToCollisionManager()->void
+	auto connectOnCollisionBehaviors()->void
 	{
-		SE::CollisionManager::addCollisionalEntities(rectangle1, rectangle2, std::bind(&MyGame::rectangleCollision, this, std::placeholders::_1, std::placeholders::_2));
+		SE::CollisionManager::addCollisionalEntities(rectangle1, rectangle2, std::bind(&MyDemoGame::rectangleCollision, this, std::placeholders::_1, std::placeholders::_2));
 	}
 
-	~MyGame()
+	auto loadTextures() ->void
 	{
-		SE::SoundManager::clean();
+		texture1 = Texture("..//..//src//demo//PublicFiles//Textures//texture1.png");
+		texture2 = Texture("..//..//src//demo//PublicFiles//Textures//texture2.png");
 	}
 
+	auto initDrawableSprite() ->void
+	{
+		rectangle1 = std::make_shared<SE::Rectangle>(SE::vec2(40, 40), SE::vec2(50, 50), texture1, 2);
+		rectangle2 = std::make_shared<SE::Rectangle>(SE::vec2(150, 40), SE::vec2(50, 50), texture2, 2);
+		Game::m_renderer->addRectangleToDrawCall(rectangle1);
+		Game::m_renderer->addRectangleToDrawCall(rectangle2);
+	}
 
+	auto loadSounds() ->void
+	{
+		SoundManager::init();
+		sound1 = std::make_shared<Sound>("sunet1", "..//..//src//demo//PublicFiles//Sounds//cat_growl.wav");
+		sound2 = std::make_shared<Sound>("sunet2", "..//..//src//demo//PublicFiles//Sounds//dog_bark.wav");
+
+		SoundManager::add(sound1);
+		SoundManager::add(sound2);
+	}
 
 private:
-	SE::Texture texture1;
-	SE::Texture texture2;
+	Texture texture1;
+	Texture texture2;
 
 	std::shared_ptr<SE::Rectangle> rectangle1;
 	std::shared_ptr<SE::Rectangle> rectangle2;
 
-	std::shared_ptr<SE::Sound> sound1;
-	std::shared_ptr<SE::Sound> sound2;
+	std::shared_ptr<Sound> sound1;
+	std::shared_ptr<Sound> sound2;
 };
-
-
 
 int main()
 {
-	MyGame myGame;
+	MyDemoGame myGame;
 	myGame.start();
-
 	return 0;
 }
